e3fcddf10a080ae8389e60a55570fdd4dac8144e
NATIA SULKHANISHVILI <96068334+nsulkhanishvili@users.noreply.github.com>
2022-06-23 16:51:41 +0400

    corect

--- davaleba-2/src/coomponents/Counter.js
+++ davaleba-2/src/coomponents/Counter.js
@@ -1,15 +1,32 @@
 import Buatton from "./Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Counter({stratValue, increase}) {
 
   const [number, setNumber] = useState(stratValue || 0);
+
+  const decrement =() => {
+    if (number > 0) {
+      setNumber(number - (increase || 5))
+    } else {
+      setNumber (0);
+    }
+  };
+
+  //  useEffect (() => {
+  //   if (stratValue) {
+  //     setNumber(stratValue);
+  //   } else {
+  //     setNumber (0);
+  //   }
+  //  }, [stratValue]);
 
     return (
       <div className="Counter">
         <p>{number}</p>
         <input 
           type="number"
+          min="0"
           placeholder="Type Number"
           value = {number} 
           onChange={(e) => setNumber(e.target.value)}
@@ -22,7 +39,7 @@ export default function Counter({stratValue, increase}) {
         <Buatton  
           className="Button"
           text='გამოკლება'
-          onClick={() => setNumber(number - (increase || 5))}/>
+          onClick={decrement}/>
       </div>
   );
 }
